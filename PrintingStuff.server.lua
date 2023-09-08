7d4fc484dd3d8f502ad310d5a68bcc9de4775c5a
rouxerssssefdsdf345345 <144142251+rouxerssssefdsdf345345@users.noreply.github.com>
2023-09-08 08:46:36 -0600

    PrintingStuff.server.lua

---
+++ PrintingStuff.server.lua
@@ -0,0 +1,13 @@
+local printerererer = "messaging thing and. "
+
+local idk = "Hello this is" ,value, "message."
+local thing = "This is a "
+
+local value = "1"
+
+print(printerererer)
+
+
+game.Players.PlayerAdded:Connect(function()
+    print(thing..printerererer..idk)
+end)
